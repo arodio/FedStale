d5914bd2312523d616826c36dd2f08a67c93146a
Daniel Richards Ravi Arputharaj <richydany@gmail.com>
2024-10-03 10:02:37 +0200

    Pushing the logs to github

--- building_availability_matrices/markov_avail_mat.py
+++ building_availability_matrices/markov_avail_mat.py
@@ -1,15 +1,14 @@
 import pandas as pd
 from markov_models.utils import gen_avail_mat
-import matplotlib.pyplot as plt
 
 CLIENTS = {
-    'C0' : (0.4, 0.6),
-    'C1' : (0.4, 0.6),
-    'C2' : (0.4, 0.6),
-    'C3' : (0.4, 0.6),
-    'C4' : (0.4, 0.6),
-    'C5' : (0.4, 0.6),
-    'C6' : (0.4, 0.6)
+    "C0": (0.4, 0.6),
+    "C1": (0.4, 0.6),
+    "C2": (0.4, 0.6),
+    "C3": (0.4, 0.6),
+    "C4": (0.4, 0.6),
+    "C5": (0.4, 0.6),
+    "C6": (0.4, 0.6),
 }
 
 # CLIENTS_CORR = {
@@ -23,22 +22,28 @@ CLIENTS = {
 # }
 
 CLIENTS_CORR = {
-    'C0' : (0.94, 0.96),
-    'C1' : (0.94, 0.96),
-    'C2' : (0.94, 0.96),
-    'C3' : (0.94, 0.96),
-    'C4' : (0.94, 0.96),
-    'C5' : (0.94, 0.96),
-    'C6' : (0.94, 0.96)
+    "C0": (0.94, 0.96),
+    "C1": (0.94, 0.96),
+    "C2": (0.94, 0.96),
+    "C3": (0.94, 0.96),
+    "C4": (0.94, 0.96),
+    "C5": (0.94, 0.96),
+    "C6": (0.94, 0.96),
 }
 
+
 def save_avail_mat(avail_mat, key_word):
-    pd.DataFrame(data=avail_mat, columns = [f't{i}' for i in range(100)], index=[f'C_{i}' for i in range(7)]).to_csv(f'building_availability_matrices/availability_matrices/av-mat-3/av-mat_markov_spots_{key_word}.csv')
+    pd.DataFrame(
+        data=avail_mat,
+        columns=[f"t{i}" for i in range(100)],
+        index=[f"C_{i}" for i in range(7)],
+    ).to_csv(
+        f"building_availability_matrices/availability_matrices/av-mat-3/av-mat_markov_spots_{key_word}.csv"
+    )
 
 
 avail_mat = gen_avail_mat(CLIENTS_CORR, 100)
-save_avail_mat(avail_mat=avail_mat, key_word='corr')
+save_avail_mat(avail_mat=avail_mat, key_word="corr")
 
-avail_mat = gen_avail_mat(CLIENTS_CORR, 100)
-save_avail_mat(avail_mat=avail_mat, key_word='uncorr')
-
+avail_mat = gen_avail_mat(CLIENTS, 100)
+save_avail_mat(avail_mat=avail_mat, key_word="uncorr")
