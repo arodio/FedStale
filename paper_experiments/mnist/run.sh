121cdea2d0c73811701c23d3456f207b5178656f
Charlotte Rodriguez <charlotte.rodriguez@inria.fr>
2024-10-08 17:28:10 +0200

    remove commented command for unknown probs argument

--- paper_experiments/mnist/run.sh
+++ paper_experiments/mnist/run.sh
@@ -44,7 +44,6 @@ echo "=> training"
 
 ### - Parameters to choose for training - ###
 ### - Only change here - ###
-# availabilities="opt-pb3-stage2"
 availabilities="markov_spots_uncorr_fine_tuning" # list of availability matrices
 fl_algo="fedavg" # list of FL algorithms
 biased="2" # 0:unbiased, 1:biased, 2:hybrid (unbiased except when all clients available)
@@ -108,36 +107,6 @@ done
 done
 fi
 
-# # # unknown participation probs:
-# # for availability in $availabilities; do
-# # availability_matrix_path="availability_matrices/availability_matrix_${availability}.csv"
-# # for heterogeneity in $heterogeneities; do
-# # for lr in $lrs; do
-# # for seed in $seeds; do
-# # echo "Run FedAvg : p ${participation}, h ${heterogeneity}, lr ${lr}, seed ${seed}"
-# # (
-# # python train.py \
-# # mnist \
-# # --n_rounds ${n_rounds} \
-# # --participation_probs 1.0 ${participation} \
-# # --unknown_participation_probs \
-# # --bz 128 \
-# # --lr ${lr} \
-# # --log_freq 1 \
-# # --device ${device} \
-# # --optimizer sgd \
-# # --server_optimizer sgd \
-# # --logs_dir logs/mnist_CI_based_availability/clients_${n_tasks}/${availability}/biased_${biased}/fedavg/unknown_participation_probs/alpha_${alpha}/lr_${lr}/seed_${seed}/rounds_${n_rounds} \
-# # --seed ${seed} \
-# # --verbose 0 \
-# # --availability_matrix_path ${availability_matrix_path} \
-# # --biased ${biased}
-# # )
-# # done
-# # done
-# # done
-# # done
-
 # ------------------------------- #
 # --- Experiments for FedVARP --- #
 # ------------------------------- #
@@ -171,36 +140,6 @@ done
 done
 done
 fi
-
-# # # unknown participation probs:
-# # for availability in $availabilities; do
-# # availability_matrix_path="availability_matrices/availability_matrix_${availability}.csv"
-# # for heterogeneity in $heterogeneities; do
-# # for lr in $lrs; do
-# # for seed in $seeds; do
-# # echo "Run FedVARP : p ${participation}, h ${heterogeneity}, lr ${lr}, seed ${seed}"
-# # (
-# # python train.py \
-# # mnist \
-# # --n_rounds ${n_rounds} \
-# # --participation_probs 1.0 ${participation} \
-# # --unknown_participation_probs \
-# # --bz 128 \
-# # --lr ${lr} \
-# # --log_freq 1 \
-# # --device ${device} \
-# # --optimizer sgd \
-# # --server_optimizer history \
-# # --logs_dir logs/mnist_CI_based_availability/clients_${n_tasks}/${availability}/biased_${biased}/fedvarp/unknown_participation_probs/alpha_${alpha}/lr_${lr}/seed_${seed}/rounds_${n_rounds} \
-# # --seed ${seed} \
-# # --verbose 0 \
-# # --availability_matrix_path ${availability_matrix_path} \
-# # --biased ${biased}
-# # )
-# # done
-# # done
-# # done
-# # done
 
 # -------------------------------- #
 # --- Experiments for FedStale --- #
@@ -238,36 +177,3 @@ done
 done
 done
 fi
-
-# # # unknown participation probs:
-# # for availability in $availabilities; do
-# # availability_matrix_path="availability_matrices/availability_matrix_${availability}.csv"
-# # for heterogeneity in $heterogeneities; do
-# # for lr in $lrs; do
-# # for weight in $weights; do
-# # for seed in $seeds ; do
-# # echo "Run FedStale : p ${participation}, h ${heterogeneity}, beta ${weight}, lr ${lr}, seed ${seed}"
-# # (
-# # python train.py \
-# # mnist \
-# # --n_rounds ${n_rounds} \
-# # --participation_probs 1.0 ${participation} \
-# # --unknown_participation_probs \
-# # --bz 128 \
-# # --lr ${lr} \
-# # --log_freq 1 \
-# # --device ${device} \
-# # --optimizer sgd \
-# # --server_optimizer history \
-# # --history_coefficient ${weight} \
-# # --logs_dir logs/mnist_CI_based_availability/clients_${n_tasks}/${availability}/biased_${biased}/fedstale/unknown_participation_probs/alpha_${alpha}/lr_${lr}/seed_${seed}/rounds_${n_rounds} \
-# # --seed ${seed} \
-# # --verbose 0 \
-# # --availability_matrix_path ${availability_matrix_path} \
-# # --biased ${biased}
-# # )
-# # done
-# # done
-# # done
-# # done
-# # done
