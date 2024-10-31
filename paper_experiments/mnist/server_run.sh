dca3d3d70a383030797d8d187747c39d8d6bd62c
Daniel Richards Ravi Arputharaj <richydany@gmail.com>
2024-10-31 17:29:34 +0100

    Changed the script file for fedvarp bz

--- paper_experiments/mnist/server_run.sh
+++ paper_experiments/mnist/server_run.sh
@@ -44,7 +44,7 @@ echo "=> training"
 
 ### - Parameters to choose for training - ###
 ### - Only change here - ###
-availabilities="gaussian-corr-ft-35 gaussian-uncorr-ft-35 gaussian-corr-ft-50 gaussian-uncorr-ft-50 gp-35-tcsc-10ft-2 gp-35-tusc-10ft-2 gp-50-tcsc-10ft-2 gp-50-tusc-10ft-2 gp-20304050607080-tcsu-10ft gp-20304050607080-tusu-10ft gp-25255050507575-tcsu-10ft gp-25255050507575-tusu-10ft gp-40404040606060-tcsu-10ft gp-40404040606060-tusu-10ft" # list of availability matrices
+availabilities="gaussian-corr-ft-35 gaussian-uncorr-ft-35 gaussian-corr-ft-50 gaussian-uncorr-ft-50 gp-35-tcsc-10ft-2 gp-35-tusc-10ft-2 gp-50-tcsc-10ft-2 gp-50-tusc-10ft-2 gp-20304050607080-tcsu-10ft gp-20304050607080-tusu-10ft gp-25255050507575-tcsu-10ft gp-25255050507575-tusu-10ft" # list of availability matrices
 fl_algo="fedavg fedvarp" # list of FL algorithms
 biased="2" # 0:unbiased, 1:biased, 2:hybrid (unbiased except when all clients available)
 fine_tuning=10 # Change this to # of finetuning step
@@ -129,7 +129,7 @@ python train.py \
 mnist \
 --n_rounds ${n_rounds} \
 --participation_probs 1.0 ${participation} \
---bz 128 \
+--bz ${batch_size} \
 --lr ${lr} \
 --log_freq 1 \
 --device ${device} \
@@ -165,7 +165,7 @@ python train.py \
 mnist \
 --n_rounds ${n_rounds} \
 --participation_probs 1.0 ${participation} \
---bz 128 \
+--bz ${batch_size} \
 --lr ${lr} \
 --log_freq 1 \
 --device ${device} \
