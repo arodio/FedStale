9044c33c41db226c3dc9892a8089c4237276d617
Daniel Richards Ravi Arputharaj <richydany@gmail.com>
2024-11-04 11:39:18 +0100

    modified file names

--- paper_experiments/mnist/oar_script.sh
+++ paper_experiments/mnist/oar_script.sh
@@ -5,3 +5,9 @@ for seed in 42 78 84;do
 		echo $seed $lr && oarsub -p "gpu='YES' and host='nefgpu52.inria.fr'" -l /gpunum=1,walltime=55 -t idempotent "./server_run.sh $seed 128"
 	done
 done
+
+for seed in 42 78 84;do
+	for lr in "1e-2" "5e-2" "1e-3";do
+		echo $seed $lr && oarsub -p "gpu='YES' and host='nefgpu52.inria.fr'" -l /gpunum=1,walltime=55 -t idempotent "./server_run.sh $seed 128"
+	done
+done
