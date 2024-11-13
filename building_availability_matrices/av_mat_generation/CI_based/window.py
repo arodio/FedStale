541639c63f266d8e1f624b8d86cfbf812f7f7084
Daniel Richards Ravi Arputharaj <richydany@gmail.com>
2024-11-13 17:04:00 +0100

    Removed Italy and added HK

--- building_availability_matrices/av_mat_generation/CI_based/window.py
+++ building_availability_matrices/av_mat_generation/CI_based/window.py
@@ -59,7 +59,7 @@ def load_data(countries=None):
         "Spain": os.path.join(folder, "ES_2022_hourly.csv"),
         "Hungary": os.path.join(folder, "HU_2022_hourly.csv"),
         "Singapore": os.path.join(folder, "SG_2022_hourly.csv"),
-        "Italy": os.path.join(folder, "IT_2022_hourly.csv"),
+        "Hong Kong": os.path.join(folder, "HK_2022_hourly.csv"),
         "Japan": os.path.join(folder, "JP_2022_hourly.csv"),
         "South Africa": os.path.join(folder, "ZA_2022_hourly.csv"),
         "Peru": os.path.join(folder, "PE_2022_hourly.csv"),
